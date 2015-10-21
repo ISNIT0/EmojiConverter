64380872892c33f747b064a0834d65f9979ae679
Joe Reeve <joe@wigwamm.com>
2015-10-21 21:42:10 +0100

    init

---
+++ server.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var emojis = require('./emojis.json'); //https://github.com/Ranks/emojione/blob/master/emoji.json
+
+app.get('/:name', function(req, res){
+  var emoji = emojis[req.params.name];
+  if(!emoji)
+    return res.status(500).send('Not a valid emoji, bro.');
+  res.send(emoji.unicode);
+});
+
+app.listen(1337);
